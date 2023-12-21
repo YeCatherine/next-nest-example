bd09e6230bdf55f2a120afd0c93a9f914a5a868a
YeKate <kate.yemelyanenka@gmail.com>
2023-12-21 10:05:59 -0800

    feat: backend - tests and dto for books

--- apps/backend/src/book/book.controller.spec.ts
+++ apps/backend/src/book/book.controller.spec.ts
@@ -4,17 +4,48 @@ import { BookService } from './book.service';
 
 describe('BookController', () => {
   let controller: BookController;
+  let service: BookService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [BookController],
-      providers: [BookService],
+      providers: [
+        {
+          provide: BookService,
+          useValue: {
+            create: jest.fn(),
+            findAll: jest.fn(),
+            findOne: jest.fn(),
+            update: jest.fn(),
+            remove: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<BookController>(BookController);
+    service = module.get<BookService>(BookService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  describe('create', () => {
+    it('should create a book', async () => {
+      const dto = { title: 'New Book', author: 'Author' };
+      service.create.mockResolvedValue(dto);
+      expect(await controller.create(dto)).toEqual(dto);
+      expect(service.create).toHaveBeenCalledWith(dto);
+    });
+  });
+
+  describe('findAll', () => {
+    it('should return an array of book', async () => {
+      const result = [{ title: 'Book One' }, { title: 'Book Two' }];
+      service.findAll.mockResolvedValue(result);
+      expect(await controller.findAll()).toEqual(result);
+    });
+  });
+
 });
--- apps/backend/src/book/dto/create-book.dto.ts
+++ apps/backend/src/book/dto/create-book.dto.ts
@@ -1,1 +1,21 @@
-export class CreateBookDto {}
+import { IsString, IsInt, IsNotEmpty, IsDate } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateBookDto {
+  @IsNotEmpty()
+  @IsString()
+  @ApiProperty({ example: 'Book Title', description: 'The title of the book' })
+  readonly title: string;
+
+  @ApiProperty({ example: '1', description: 'Id of Author' })
+  @IsInt()
+  readonly authorId: number;
+
+  @ApiProperty({ example: '100', description: 'Amound of pages' })
+  @IsInt()
+  readonly pageCount: number;
+
+  @ApiProperty({ example: '2023-01-01', description: 'The release date' })
+  @IsDate()
+  readonly releaseDate: Date;
+}
