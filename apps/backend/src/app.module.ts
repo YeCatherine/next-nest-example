d35e4b7da7bb47c023a2a74c25e94b63a0d48e83
YeKate <kate.yemelyanenka@gmail.com>
2023-12-20 23:01:00 -0800

    feat: generated author and book resources

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthorModule } from './author/author.module';
+import { BookModule } from './book/book.module';
 
 @Module({
-  imports: [],
+  imports: [AuthorModule, BookModule],
   controllers: [AppController],
   providers: [AppService],
 })
