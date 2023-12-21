d1071be1313d7f7994a22ab2da47838d42724e21
YeKate <kate.yemelyanenka@gmail.com>
2023-12-21 01:00:40 -0800

    refactor: content list improved documentation and code splitting

--- apps/frontend/src/app/book/page.tsx
+++ apps/frontend/src/app/book/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ContentItemPage from '@/components/ContentList';
 
-export default function BooksPage() {
+export default function BookPage() {
   return <ContentItemPage entity={"book"} titleFields={["title"]} />;
 }
--- apps/frontend/src/components/ContentList.tsx
+++ apps/frontend/src/components/ContentList.tsx
@@ -1,99 +1,142 @@
 'use client';
-import React, { useState, useEffect, useMemo } from 'react';
+import React, { useState, useEffect, useMemo, FC } from 'react';
 import axios from 'axios';
 import Link from 'next/link';
 
-function capitalizeFirstLetter(string) {
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+
+/**
+ * Capitalizes the first letter of the given string.
+ * 
+ * @param {string} string - The string to capitalize.
+ * 
+ * @returns {string} The capitalized string.
+ */
+function capitalizeFirstLetter(string: string): string {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+interface ContentItem {
+  id: string;
+  [key: string]: any;
+}
 
-const ContentItemPage = ({ entity = 'author', titleFields = ['title'] }) => {
-  const [contentItems, setContentItems] = useState<Any[]>([]);
-  const [error, setError] = useState(null);
+interface ContentItemPageProps {
+  entity?: string;
+  titleFields?: string[];
+}
+
+/**
+ * Renders the page for a specific type of content item.
+ *
+ * @param {ContentItemPageProps} props - The properties of the component.
+ *
+ * @returns {React.ReactElement} The React component.
+ */
+const ContentItemPage: FC<ContentItemPageProps> = ({ entity = 'author', titleFields = ['title'] }) => {
+  const [contentItems, setContentItems] = useState<ContentItem[]>([]);
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        console.log({ API_BASE_URL }, `${API_BASE_URL}/${entity}`);
         const result = await axios(`${API_BASE_URL}/${entity}`);
-        console.log('result.data',result.data);
         setContentItems(result.data.data);
-      } catch (err) {
-        setError(err);
+      } catch (err: any) { // Use AxiosError for more specific error handling
+        setError(err.message);
       }
     };
 
     fetchData();
   }, [entity]);
 
-  const contentItemTitle = useMemo(() => {
-    if (contentItems.length === 0) {
-      // Return an empty array or a placeholder value if contentItems is empty
-      return [];
-    }
+  const contentItemTitle = useMemo(() => contentItems?.map((item) =>
+    titleFields.map((field, index) => (
+      <React.Fragment key={index}>
+        {index > 0 ? ' ' : ''}
+        {item[field]}
+      </React.Fragment>
+    )),
+  ), [contentItems, titleFields]);
 
-    return contentItems.map((item) =>
-      titleFields.map((field, index) => (
-        <React.Fragment key={index}>
-          {index > 0 ? ' ' : ''}
-          {item[field]}
-        </React.Fragment>
-      ))
-    );
-
-  }, [contentItems, titleFields]);
-
-  console.log('contentItems:', { contentItems });
-if(contentItems.length === 0){
-    return 'empty';
-}
   return (
     <div className="max-w-4xl mx-auto p-4">
-      <div className="">
-        <div className="flex flex-row justify-between">
-          <h1 className="text-2xl font-bold text-gray-800 mb-4">
-            {capitalizeFirstLetter(entity)}
-          </h1>
-          <Link
-            href={`/${entity}/create`}
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          >
-            Create new content item
-          </Link>
-        </div>
-      </div>
-      {error && <p>Error loading content items.</p>}
-      {contentItems.length > 0 ? (
-        contentItems.map((contentItem) => (
-          <div
-            key={contentItem.id}
-            className="border-b py-2 flex flex-row justify-between"
-          >
-            <Link
-              href={`/${entity}/${contentItem.id}`}
-              className="text-blue-600 hover:text-blue-800 transition duration-300"
-            >
-              {contentItemTitle}
-            </Link>
-            <div className="flex flex-row">
-              <Link href={`/${entity}/${contentItem.id}/edit`} className="p-2">
-                ✏️
-              </Link>
-              <Link
-                href={`/${entity}/${contentItem.id}/delete`}
-                className="p-2"
-              >
-                🗑
-              </Link>
-            </div>
-          </div>
-        ))
-      ) : (
-        <p>No content items available.</p>
-      )}
+      <ContentListTopBar entity={entity} />
+      {error && <p className="text-red-500">Error loading content items: {error}</p>}
+      <ContentListItems entity={entity} contentItems={contentItems} contentItemTitle={contentItemTitle} />
     </div>
+  );
+};
+
+interface ContentListTopBarProps {
+  entity: string;
+}
+
+/**
+ * Displays the top bar of the content list.
+ * 
+ * @param {ContentListTopBarProps} props - The properties of the component.
+ * 
+ * @returns {React.ReactElement} The React component.
+ */
+const ContentListTopBar: FC<ContentListTopBarProps> = ({ entity }) => (
+  <div className="flex flex-row justify-between">
+    <h1 className="text-2xl font-bold text-gray-800 mb-4">
+      {capitalizeFirstLetter(entity)}
+    </h1>
+    <Link
+      href={`/${entity}/create`}
+      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+    >
+      Create new content item
+    </Link>
+  </div>
+);
+
+interface ContentListItemsProps {
+  entity: string;
+  contentItems: ContentItem[];
+  contentItemTitle: React.ReactNode[];
+}
+
+/**
+ * Lists the content items.
+ * 
+ * @param {ContentListItemsProps} props - The properties of the component.
+ * 
+ * @returns {React.ReactElement} The React component.
+ */
+const ContentListItems: FC<ContentListItemsProps> = ({ entity, contentItems=[], contentItemTitle }) => {
+  console.log('test:',{contentItems})
+
+  if (contentItems.length === 0) {
+    return <p>No content items available.</p>;
+  }
+
+  return (
+    <>
+      {contentItems.map((contentItem) => (
+        <div
+          key={contentItem.id}
+          className="border-b py-2 flex flex-row justify-between"
+        >
+          <Link
+            href={`/${entity}/${contentItem.id}`}
+            className="text-blue-600 hover:text-blue-800 transition duration-300"
+          >
+            {contentItemTitle}
+          </Link>
+          <div className="flex flex-row">
+            <Link href={`/${entity}/${contentItem.id}/edit`} className="p-2">
+              ✏️
+            </Link>
+            <Link href={`/${entity}/${contentItem.id}/delete`} className="p-2">
+              🗑
+            </Link>
+          </div>
+        </div>
+      ))}
+    </>
   );
 };
 
